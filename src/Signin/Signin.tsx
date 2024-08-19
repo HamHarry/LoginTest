c2a679c45698d5daa19e3586bbbbec4c1f753ce6
HamHarry <hamza_2641@hotmail.com>
2024-08-19 21:14:22 +0700

    signIn-design

--- src/Signin/Signin.tsx
+++ src/Signin/Signin.tsx
@@ -1,30 +1,106 @@
+import { useNavigate } from "react-router-dom";
+import "./Signin.css";
+import { useState } from "react";
+
 const Signin = () => {
+  const [fname, setFname] = useState<string>("");
+  const [lname, setLname] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [conPassword, setConPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handlefname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const valuefname = e.target.value;
+    setFname(valuefname);
+  };
+  const handlelname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const valuelname = e.target.value;
+    setLname(valuelname);
+  };
+  const handleusername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const valuefname = e.target.value;
+    setUsername(valuefname);
+  };
+  const handlepassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const valuefname = e.target.value;
+    setPassword(valuefname);
+  };
+  const handleemail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const valuefname = e.target.value;
+    setEmail(valuefname);
+  };
+
+  const handlesubmit = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    try {
+      const item = {
+        fname: fname,
+        lname: lname,
+        username: username,
+        password: password,
+        email: email,
+      };
+      navigate("/");
+      console.log(item);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="signin">
-      <form>
+      <form className="warp-signin">
+        <h1>Sign In</h1>
         <div className="first-name">
-          <label htmlFor="fname">Frist Name: </label>
-          <input type="text" placeholder="first name..." />
+          <label htmlFor="fname">Frist Name</label>
+          <input
+            type="text"
+            placeholder="first name..."
+            onChange={handlefname}
+          />
         </div>
         <div className="last-name">
           <label htmlFor="lname">Last Name: </label>
-          <input type="text" placeholder="last name..." />
+          <input
+            type="text"
+            placeholder="last name..."
+            onChange={handlelname}
+          />
         </div>
         <div className="username">
-          <label htmlFor="user">Username: </label>
-          <input type="text" placeholder="username..." />
+          <label htmlFor="user">Username</label>
+          <input
+            type="text"
+            placeholder="username..."
+            onChange={handleusername}
+          />
         </div>
         <div className="password">
-          <label htmlFor="pass">Password: </label>
-          <input type="text" placeholder="password..." />
-        </div>
-        <div className="con-password">
-          <label htmlFor="con-pass">Confirm Password: </label>
-          <input type="text" placeholder="confirm-password..." />
+          <label htmlFor="pass">Password</label>
+          <input
+            type="text"
+            placeholder="password..."
+            onChange={handlepassword}
+          />
         </div>
         <div className="email">
-          <label htmlFor="email">Email: </label>
-          <input type="text" placeholder="email..." />
+          <label htmlFor="email">Email</label>
+          <input type="text" placeholder="email..." onChange={handleemail} />
+        </div>
+        <div className="btn">
+          <div className="btn-signup" onClick={handlesubmit}>
+            <p>Sign UP</p>
+          </div>
+          <div
+            className="btn-login"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <p>Login</p>
+          </div>
         </div>
       </form>
     </div>
